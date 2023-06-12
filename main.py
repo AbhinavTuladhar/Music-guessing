7047ed261677bf8fc3b69ac8f1b4b3fd900d8bb3
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-06-12 19:01:17 +0545

    refactor+feat: use OOP and feat. to replay clips

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import random
 import time
+from textwrap import dedent
 
 # For resolving the issue with missing dll file while importing the vlc module.
 os.add_dll_directory(r'C:\Program Files\VideoLAN\VLC')
@@ -10,39 +11,53 @@ import vlc
 # CHANGE TO YOUR PATH HERE
 os.chdir(r'E:\MUSIC\Something')
 
-GUESSING_TIME: int = 3
+def print_menu() -> None:
+    message = dedent("""
+        Choices:
+        1. Play clip again
+        2. Play another clip and reveal the current music track name
+        3. Quit
+    """)
+    print(message)
 
-def play_random_segment(file_path: str, segment_length: int = 5) -> None:
-    """
-    Plays a random `segment_length` second clop of the music file `file_path`.
+class MusicPlayer:
+    def __init__(self, segment_length: int|float=5):
+        self.segment_length = segment_length
+    
+    def construct_segment(self, file_path: str) -> None:
+        """
+        Constructs a `segment_length` second segment of the music file given by `file_path`.
+        
+        Arguments:
+            file_path: The relative or absolute path of the music file.
+        """
+        # Create VLC media player instance
+        self.player = vlc.MediaPlayer()
 
-    file_path: The absolute or relative path of the music file to be played.
-    segment_length: The number of seconds to play.
-    """
-    # Create VLC media player instance
-    player = vlc.MediaPlayer()
+        # Load the mp3 file
+        self.media = vlc.Media(file_path)
+        self.player.set_media(self.media)
 
-    # Load the mp3 file
-    media = vlc.Media(file_path)
-    player.set_media(media)
+        # Get the file duration in milliseconds
+        # Needs to played for a few milliseconds otherwise the duration becomes -1
+        self.player.play()
+        time.sleep(0.5)
+        duration = self.player.get_length()
+        self.player.stop()
 
-    # Get the file duration in milliseconds
-    # Needs to played for a few milliseconds otherwise the duration becomes -1
-    player.play()
-    time.sleep(0.5)
-    duration = player.get_length()
-    player.stop()
-
-    # Calculate the start time of the random segment
-    start_time = random.randint(0, duration - segment_length * 1000)
-    media.add_option(f":start-time={start_time // 1000}")
-    media.add_option(f":stop-time={(start_time + segment_length * 1000) // 1000}")
-
-    # Now play the file.
-    player.set_media(media)
-    player.play()
-    time.sleep(segment_length)
-    player.stop()
+        # Calculate the start time of the random segment
+        start_time = random.randint(0, duration - self.segment_length * 1000)
+        self.media.add_option(f":start-time={start_time // 1000}")
+        self.media.add_option(f":stop-time={(start_time + self.segment_length * 1000) // 1000}")
+        
+    def play_file_segment(self) -> None:
+        """
+        Plays the constructed music segment.
+        """
+        self.player.set_media(self.media)
+        self.player.play()
+        time.sleep(self.segment_length)
+        self.player.stop()
   
 if __name__ == '__main__':
     # Find the name of all the files.
@@ -53,6 +68,8 @@ if __name__ == '__main__':
                 continue
             file_name = os.path.join(root, file)
             media_files.append(file_name)
+            
+    player = MusicPlayer(segment_length=1)
   
     while True:
         # Now get an random file name and format it to get the file name from the absolute path.
@@ -60,21 +77,29 @@ if __name__ == '__main__':
         media_file_name = media_file.split('.')[0].split('\\')[-1]
 
         # Now play the random segment.
-        play_random_segment(file_path=media_file, segment_length=1)
+        player.construct_segment(file_path=media_file)
+        player.play_file_segment()
 
-        print(f'You have {GUESSING_TIME} seconds to guess the name!')
-        time.sleep(GUESSING_TIME)
-
-        print(f'The name of the track is \'{media_file_name}\'')
-
+        # Display the menu once per track.
+        print_menu()
+        
         choice = None
-        while choice not in ['Y', 'N']:
-            choice = input("Play again? (Y/N): ").strip().upper()
-            if choice == 'N':
-                print('')
+        while choice not in ['1', '2', '3']:
+            choice = input("\nEnter your choice: ")
+            
+            if choice == '1':
+                print('Ok, playing the clip again...')
+                player.play_file_segment()
+                choice = None
+            elif choice == '3':
                 break
-            elif choice != 'Y':
-                print("Invalid choice. Please enter Y or N.")
+            elif choice != '2':
+                print('invalid entry.')
+                
+        print(f'The name of the track is \'{media_file_name}\'')
+        if choice != '3':
+            input('Press enter to move on! ')
 
-        if choice == 'N':
+        if choice == '3':
+            print('Exiting...')
             break
